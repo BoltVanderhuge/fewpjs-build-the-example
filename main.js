5aca1f88e8711a976b57193a928ffdae9f3f6d77
john_wisneski <john.j.wisneski@gmail.com>
2021-02-05 19:50:03 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const body = document.querySelector("body")
 
+body.addEventListener("click", function(e){
+  const likeBox = e.target.closest("article.media-post")
+  if (e.target.className === 'like-glyph') {
+    const likeGlyph = likeBox.querySelector("span.like-glyph")
+    const likeLine = likeBox.querySelector("li")
+    mimicServerCall("bogusUrl")
+    .then(function(serverMessage){
+      if (likeGlyph.innerText != FULL_HEART){
+        likeGlyph.innerText = FULL_HEART
+        // likeLine.style.color = "red"
+        likeLine.className = "activated-heart"
+      } else {
+        likeGlyph.innerText = EMPTY_HEART
+        // likeLine.style.color = "#AAB8C2"
+        likeLine.className = "like"
+      }
+    })
+    .catch(function(error) {
+      document.getElementById("modal").className = "";
+      setTimeout(function(){ 
+        document.getElementById("modal").className = "hidden";
+      }, 5000);
+    });
+  }
+})
 
 
 
